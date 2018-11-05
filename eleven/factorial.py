8d1b6e46e425f21024294ce142dba0ed27e05e94
罗伶颖 <luolingying@ruigushop.com>
2018-11-05 13:49:00 +0800

    递归

---
+++ eleven/factorial.py
@@ -0,0 +1,6 @@
+def factorial(x):
+    res = n
+    for i in range(1, x):
+        res *= i
+    return res
+print(factorial(5))
--- seventh/euclidean.py
+++ seventh/euclidean.py
@@ -4,4 +4,33 @@ def gcd(x, y):
     else:
         return gcd(y, x%y)
 
-print(gcd(7890, 123456))
+print(gcd(8, 12))
+
+# 辗转相除法
+# 如果x小于y, x%y = x, 所以会变成gcd(y,x) 大的数会始终跑到前面
+'''
+gcd(12,8)
+gcd(8,4)
+gcd(4,0)
+4
+
+gcd(15,2)
+gcd(2,1)
+gcd(1,0)
+1
+
+gcd(15,3)
+gcd(3,0)
+3
+
+gcd(100,30)
+gcd(30,10)
+gcd(10,0)
+10
+
+gcd(20,7)
+gcd(7,6)
+gcd(6,1)
+gcd(1,0)
+1
+'''
