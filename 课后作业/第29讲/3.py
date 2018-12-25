48596b887b8cdd3dfb373619f8aa3a5c20edc7b9
罗伶颖 <luolingying@ruigushop.com>
2018-12-25 10:56:33 +0800

    文件系统

--- 课后作业/第29讲/3.py
+++ 课后作业/第29讲/3.py
@@ -7,3 +7,8 @@ for eachline in f:
     print(eachline)
     if i == num:
         break
+
+# 除了用for循环文件读取每行 还可以用readline一行一行的读取文件
+# 且如果readline在循环里面，会每个循环读取一行 一行一行读下去
+# 有一个指针，循环一次往下走一行
+        
--- 课后作业/第29讲/4.py
+++ 课后作业/第29讲/4.py
@@ -1,12 +1,21 @@
 name = input('请输入要读取的文件名:')
 numstr = input('请输入要读取的行数【格式2:5 :4 2:】')
 
+print('文件%s' % name, end = '')
 (start, end) = numstr.split(':', 1)
-if start == '':
+if start == '' and end == '':
+    print('全文的内容如下：')
     start = 0
-if end == '':
     end = 1000
-
+elif start == '':
+    print('从开始到%s行的内容是：' % end)
+    start = 0
+elif end == '':
+    print('从%s行到末尾的内容是：' % start)
+    end = 1000
+else:
+    print('从%s行到%s行的内容是：' % (start, end))
+    
 i = 0
 f = open(name)
 for eachline in f:
@@ -15,3 +24,6 @@ for eachline in f:
         print(eachline)
     if i == int(end):
         break
+
+# 字符串输出，如果有未知数的替换 一个的话在%后放一个值 两个及两个以上的话在后面跟一个序列
+# f.read()读取剩余的所有字符，然后作为字符串返回 也就是读取剩余的所有 而不是一行一行的读取
